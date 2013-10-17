0a628786c1c6aeaa57b9afd13061945863c97b82
Sonia <carlos@ubuntu.(none)>
2013-10-17 19:43:48 +0100

    modificando

--- configure.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key       = 'HP9Sqf3tuqBnj4rRnnvoOA'
-  config.consumer_secret    = '0bWoRHMvpAWpZjGisk7aDYHNmoeeex78xxEdKdkupc'
-  config.oauth_token        = "256768353-w3QjIeJSleoQAUKsRk7NREJCUzi2obMru0WgRr8c"
-  config.oauth_token_secret = "FWUxfRS33YuL1BC7QBwClqdZMPkzMoZWWqIUNxLTJg"
-end
